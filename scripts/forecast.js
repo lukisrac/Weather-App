de895f642a637e14471d6473f7a181e63ca5102b
Lukáš Rác <lukiss482@gmail.com>
2019-08-19 18:09:25 +0200

    19/08/2019 - Changed API URLs to https

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,21 +2,21 @@
 const key = 'byiDzZcJWh4wk8QAeYClPkayXAXcl6PU';
 
 const getForecast = async id => {
-  const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${key}&language=cs-CZ&details=true&metric=true`;
+  const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${key}&language=cs-CZ&details=true&metric=true`;
   const response = await fetch(url);
   const data = response.json();
   return data;
 };
 
 const getCurrentWeather = async id => {
-  const url = `http://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${key}&language=cs&details=true`;
+  const url = `https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${key}&language=cs&details=true`;
   const response = await fetch(url);
   const data = await response.json();
   return data[0];
 };
 
 const getCity = async city => {
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`;
+  const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`;
   const response = await fetch(url);
   const data = await response.json();
   return data[0];
