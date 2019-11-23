028c813fc5254d73b0283edde0645e4181453ed5
Lukáš Rác <lukiss482@gmail.com>
2019-11-23 19:36:07 +0100

    Reorganized JS files to modules

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import { getCity, getCurrentWeather, getForecast } from './forecast.js';
+
 const container = document.querySelector('.container');
 const form = document.querySelector('form');
 const thumb = document.querySelector('.thumb');
@@ -9,12 +11,28 @@ const week = document.querySelector('.forecast__wrapper');
 const updateUI = data => {
   const { cityDetails, currentWeather, forecast } = data;
 
-  const today = new Date(forecast.DailyForecasts[0].Date).toLocaleDateString('en', { weekday: 'long' });
-  const todayDate = new Date(forecast.DailyForecasts[0].Date).toLocaleDateString('en', { day: 'numeric', month: 'long', year: 'numeric' });
-  const day1 = new Date(forecast.DailyForecasts[1].Date).toLocaleDateString('en', { weekday: 'short' });
-  const day2 = new Date(forecast.DailyForecasts[2].Date).toLocaleDateString('en', { weekday: 'short' });
-  const day3 = new Date(forecast.DailyForecasts[3].Date).toLocaleDateString('en', { weekday: 'short' });
-  const day4 = new Date(forecast.DailyForecasts[4].Date).toLocaleDateString('en', { weekday: 'short' });
+  const today = new Date(
+    forecast.DailyForecasts[0].Date
+  ).toLocaleDateString('en', { weekday: 'long' });
+  const todayDate = new Date(
+    forecast.DailyForecasts[0].Date
+  ).toLocaleDateString('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const day1 = new Date(
+    forecast.DailyForecasts[1].Date
+  ).toLocaleDateString('en', { weekday: 'short' });
+  const day2 = new Date(
+    forecast.DailyForecasts[2].Date
+  ).toLocaleDateString('en', { weekday: 'short' });
+  const day3 = new Date(
+    forecast.DailyForecasts[3].Date
+  ).toLocaleDateString('en', { weekday: 'short' });
+  const day4 = new Date(
+    forecast.DailyForecasts[4].Date
+  ).toLocaleDateString('en', { weekday: 'short' });
 
   thumb.innerHTML = `
   <div class="day">${today}</div>
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,31 +1,22 @@
-/* eslint-disable no-unused-vars */
 const key = 'byiDzZcJWh4wk8QAeYClPkayXAXcl6PU';
 
-const getForecast = async id => {
+export const getForecast = async id => {
   const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${key}&details=true&metric=true`;
   const response = await fetch(url);
   const data = response.json();
   return data;
 };
 
-const getCurrentWeather = async id => {
+export const getCurrentWeather = async id => {
   const url = `https://dataservice.accuweather.com/currentconditions/v1/${id}?apikey=${key}&details=true`;
   const response = await fetch(url);
   const data = await response.json();
   return data[0];
 };
 
-const getCity = async city => {
+export const getCity = async city => {
   const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`;
   const response = await fetch(url);
   const data = await response.json();
   return data[0];
 };
-
-/* getCity('prague')
-  .then(data => {
-    console.log(data);
-    return getForecast(data.Key);
-  })
-  .then(data => console.log(data))
-  .catch(err => console.log(err)); */
